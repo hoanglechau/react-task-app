e2585d141c1001096416fdb01d1ff162eccf5015
hoanglechau <hoangchau612@gmail.com>
2023-02-27 13:48:17 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
 import React, { Component } from 'react';
+import Overview from './components/Overview';
+import uniqid from 'uniqid';
 
 class App extends Component {
 	constructor() {
 		super();
 
 		this.state = {
-			task: { text: '' },
+			task: { text: '', id: uniqid() },
 			tasks: [],
 		};
 	}
 
+	handleChange = (e) => {
+		this.setState({
+			task: {
+				text: e.target.value,
+				id: this.state.task.id,
+			},
+		});
+	};
+
+	onSubmitTask = (e) => {
+		e.preventDefault();
+		this.setState({
+			tasks: this.state.tasks.concat(this.state.task),
+			task: { text: '', id: uniqid() },
+		});
+	};
 	render() {
 		const { task, tasks } = this.state;
 
 		return (
 			<div>
-				<form>
+				<form onSubmit={this.onSubmitTask}>
 					<label htmlFor="taskInput">Enter task</label>
-					<input type="text" id="taskInput"></input>
+					<input
+						type="text"
+						id="taskInput"
+						onChange={this.handleChange}
+						value={task.text}
+					></input>
 					<button type="submit">Add Task</button>
 				</form>
+				<Overview tasks={tasks}></Overview>
 			</div>
 		);
 	}
